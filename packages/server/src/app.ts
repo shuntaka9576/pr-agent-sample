98771ad64f75f8b6d31a080e91e8be99bb8bec9c
shuntaka9576 <shuntaka9576@gmail.com>
2024-11-06 12:43:40 +0900

    Add Hono API

---
+++ packages/server/src/app.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { handle } from "hono/aws-lambda";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello from Hono + Lambda!",
+  });
+});
+
+export const handler = handle(app);
